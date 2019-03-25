4a43094184956a4e177e1750e0361ed3657b67ef
ckxz105 <liu494@usc.edu>
2019-03-25 14:57:45 -0700

    remove plyvel

--- rltk/io/adapter/__init__.py
+++ rltk/io/adapter/__init__.py
@@ -7,4 +7,4 @@ from rltk.io.adapter.hbase_key_value_adapter import HBaseKeyValueAdapter
 from rltk.io.adapter.key_set_adapter import KeySetAdapter
 from rltk.io.adapter.memory_key_set_adapter import MemoryKeySetAdapter
 from rltk.io.adapter.redis_key_set_adapter import RedisKeySetAdapter
-from rltk.io.adapter.leveldb_key_set_adapter import LevelDbKeySetAdapter
+# from rltk.io.adapter.leveldb_key_set_adapter import LevelDbKeySetAdapter
